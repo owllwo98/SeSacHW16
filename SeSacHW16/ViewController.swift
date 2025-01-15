87cbc5efbf501872ea767d53a97c8133c52a7fc3
owllwo98 <sloopy98@naver.com>
2025-01-15 15:57:30 +0900

    Initial Commit

---
+++ SeSacHW16/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSacHW16
+//
+//  Created by 변정훈 on 1/15/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
